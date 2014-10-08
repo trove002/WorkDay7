2c6652b69152a8bb8a84f52b58390ddd05d85abc
idsc3101 <idsc3101@gmail.com>
2014-10-08 17:11:42 -0500

    i was here!!!

--- PopulationYear/PopulationYear/Form1.vb
+++ PopulationYear/PopulationYear/Form1.vb
@@ -3,7 +3,15 @@
 Public Class Form1
 
     Private Sub btnPopulation_Click(sender As Object, e As EventArgs) Handles btnPopulation.Click
-        MessageBox.Show("The answer: not implemented yet!")
+        Dim pop As Double = 7000000000
+        Dim year As Integer = 2014
+
+        Do While pop >= 6000000
+            pop = pop / 2
+            year = year - 50
+        Loop
+
+        MessageBox.Show("The answer: The population was 6,000,000 in " & CStr(year))
 
     End Sub
 End Class
